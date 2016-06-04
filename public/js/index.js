c2f953578d4c636abd2b7497cd62b3260b0f4d5a
Mohamed Bouchiba <mohamed.bouchiba1993@gmail.com>
2016-06-05 01:28:18 +0200

    Section 2 40%

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,9 @@
 /**
  * Created by Bouchiba on 14/05/2016.
  */
-$(' li > a').click(function() {
+$('#BarDeNavigation > ul > li > a').click(function() {
     $('li').removeClass();
     $(this).parent().css('background-color','#B5A183 !important');
     $(this).parent().addClass('active_custom');
 });
+
